d8326cc06bd8d88624efa2491952b00a97c97769
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-06-18 12:07:09 -0400

    Utilizing given 3rd homework HTML and CSS; Added initial comments for JS

---
+++ 02-Homework/Develop/script.js
@@ -0,0 +1,24 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+//PROMPT 1: User choose password length longer than 8 chars but shorter than 128 chars
+
+//PROMPT 2: Lowercase chars?
+//PROMPT 3: Uppercase chars?
+//PROMPT 4: Numeric chars?
+//PROMPT 5: Special chars?
+
+// Write password to page
+
